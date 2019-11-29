f5025b99d7752091fa14f51f7babf3147af0e23d
deFaup <gregoire.df@hotmail.fr>
2019-11-28 18:02:19 -0600

    poicare callable through handle_poincare

--- poincare.py
+++ poincare.py
@@ -40,17 +40,59 @@ def calculate_singularities(im, angles, tolerance, W):
     draw = ImageDraw.Draw(result)
 
     colors = {"loop" : (150, 0, 0), "delta" : (0, 150, 0), "whorl": (0, 0, 150)}
+    map_ = {}
+    map_['delta']= []
+    map_['loop']= []
 
     for i in range(1, len(angles) - 1):
         for j in range(1, len(angles[i]) - 1):
             singularity = poincare_index_at(i, j, angles, tolerance)
             if singularity != "none":
-                draw.ellipse([(i * W, j * W), ((i + 1) * W, (j + 1) * W)], outline = colors[singularity])
+                vertical_axis_left = (i * W, j * W)
+                horizontal_axis_bottom = ((i + 1) * W, (j + 1) * W)
 
+                draw.rectangle([vertical_axis_left, horizontal_axis_bottom], outline = colors[singularity])
+                map_[singularity].append((i * W, j * W, (i + 1) * W, (j + 1) * W))
+    
     del draw
+    return result, map_
 
-    return result
+def handle_poincare(image,block_size,tolerance,smooth,save):
+    """ Just a handle to call poincare from other python file instead of CLI
+        smooth and save are boolean
+        image = path to image
+        block_size = smallest window to look at
+        tolerance = ?
+    """
 
+    im = Image.open(image)
+    im = im.convert("L")  # covert to grayscale
+
+    ##TO DO
+    # add some image processing here
+    # gray = cv2.imread(member_path + "/" + image, cv2.IMREAD_GRAYSCALE)
+    # retval, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
+    # cv2.imwrite(dataset_path +  "/" + member + "/_" + image.replace('.tif','bin.tif'), binary)
+
+    W = int(block_size)
+
+    f = lambda x, y: 2 * x * y
+    g = lambda x, y: x ** 2 - y ** 2
+
+    angles = utils.calculate_angles(im, W, f, g)
+    if smooth:
+        angles = utils.smooth_angles(angles)
+
+    result, map_ = calculate_singularities(im, angles, int(args.tolerance[0]), W)
+    result.show()
+
+    if save:
+        base_image_name = os.path.splitext(os.path.basename(args.image[0]))[0]
+        result.save(base_image_name + "_poincare.gif", "GIF")
+
+    return map_
+
+# If run from CLI
 parser = argparse.ArgumentParser(description="Singularities with Poincare index")
 parser.add_argument("image", nargs=1, help = "Path to image")
 parser.add_argument("block_size", nargs=1, help = "Block size")
@@ -59,21 +101,4 @@ parser.add_argument('--smooth', "-s", action='store_true', help = "Use Gauss for
 parser.add_argument("--save", action='store_true', help = "Save result image as src_poincare.gif")
 args = parser.parse_args()
 
-im = Image.open(args.image[0])
-im = im.convert("L")  # covert to grayscale
-
-W = int(args.block_size[0])
-
-f = lambda x, y: 2 * x * y
-g = lambda x, y: x ** 2 - y ** 2
-
-angles = utils.calculate_angles(im, W, f, g)
-if args.smooth:
-    angles = utils.smooth_angles(angles)
-
-result = calculate_singularities(im, angles, int(args.tolerance[0]), W)
-result.show()
-
-if args.save:
-    base_image_name = os.path.splitext(os.path.basename(args.image[0]))[0]
-    result.save(base_image_name + "_poincare.gif", "GIF")
+handle_poincare(args.image[0],args.block_size[0],args.tolerance[0],args.smooth,args.save)
