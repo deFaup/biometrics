992003120bd7e14f9338bb703a476abf7782df10
deFaup <gregoire.df@hotmail.fr>
2019-11-29 20:07:51 -0600

    sample main

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
 from enroll import Enroll
+import os
 
-######### Test BEGIN
-from poincare import handle_poincare
-from delta_core_processing import get_distance
 
-map_,im_shape = handle_poincare("/home/greg/Documents/Biometrics/final_project/fvc2004/DB3_B/101_1.tif",16, 1, True, False)
-print("map of cores and deltas")
-print(map_)
+# Take each person in the database
+# Enroll his/her information if compatible with our algorithm
+# Save the result in the output path: a new folder is created
+DB_path = "/home/greg/Documents/Biometrics/final_project/testDB/"
+output_path = os.getcwd()+"/output3/"
 
-#im_shape is (width,height) #(300,480 for FVC2002 and 2004)
-center = (im_shape[0]/2,im_shape[1]/2)
-print("center:")
-print(center)
+smooth = True
+save_poincare = False #saving image with all cores and deltas detected by poincare
+show_poincare = False
+save_dist = True
+show_dist = True
 
-dist,sign = get_distance(map_, center)
-print("dist,sign")
-print(dist)
-print(sign)
-######### Test END
+DB = Enroll(DB_path, output_path, smooth, save_poincare, show_poincare, save_dist, show_dist)
 
-#DB_path = "/home/greg/Documents/Biometrics/final_project/fvc2004/"
 
-# Take each person in the database and enroll his infomation if compatible with our algorithm
-#DB = Enroll(DB_path)
+
 
